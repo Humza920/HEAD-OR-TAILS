41fdd73e1d1937529143c84c7e317265e4565523
Humza920 <humza.rehman.779@gmail.com>
2024-04-23 13:47:46 -0700

    HALF

---
+++ app.js
@@ -0,0 +1,15 @@
+// const heads = document.querySelector(".head")
+// const tails = document.querySelector(".tail")
+
+function toss() {
+const logic = Math.ceil(Math.random()*2)
+console.log(logic);
+const heads = 1
+const tails = 2
+
+if (logic==heads) {
+
+} 
+else
+    {console.log("You loss");}
+}
